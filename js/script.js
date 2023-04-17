1453c34910d96a30507daa340e16dc6da30b7b5b
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-17 15:16:31 +0200

    add pseudocodifica

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// MAIL
+
+/*
+
+Creare un array con varie mail
+Chiedere all'utente la mail tramite prompt
+SE mail è presente nell'array 
+    stampa Mail presente
+ALTRIMENTI 
+    stampa Mail non presente nel database.
+FINE SE
+
+*/
