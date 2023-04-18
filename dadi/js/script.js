162d9d09708ab792df0baef1d8e91b905aa2fe7e
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-18 10:13:23 +0200

    add button function

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -18,19 +18,34 @@ const eleScoreUser = document.querySelector('#score-player');
 const eleScoreComputer = document.querySelector('#score-computer');
 const eleOutput = document.querySelector('.output')
 
-const userNumber = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
-eleScoreUser.innerHTML = userNumber;
-console.log(`Questo è il numero random dell'utente: ` + userNumber);
 
-const computerNumber = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
-eleScoreComputer.innerHTML = computerNumber;
-console.log(`Questo è il numero random del computer: ` + computerNumber);
 
-if (userNumber > computerNumber) {
-    console.log(`L'utente ha vinto! il numero ${userNumber} è maggiore di ${computerNumber}`);
-} else if (computerNumber > userNumber) {
-    console.log(`Il computer ha vinto! il numero ${computerNumber} è maggiore di ${userNumber}`);
-} else {
-    console.log(`Non ha vinto nessuno! Il numero ${userNumber} è uguale al numero ${computerNumber}`);
-}
+eleBtnPlay.addEventListener('click', 
+    function () {
+        const userNumber = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
+        eleScoreUser.innerHTML = userNumber;
+        console.log(`Questo è il numero random dell'utente: ` + userNumber);
+        
+        const computerNumber = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
+        eleScoreComputer.innerHTML = computerNumber;
+        console.log(`Questo è il numero random del computer: ` + computerNumber);
+        
+        let message
+        
+        if (userNumber > computerNumber) {
+            message = `L'utente ha vinto!`
+            console.log(`L'utente ha vinto! il numero ${userNumber} è maggiore di ${computerNumber}`);
+        } else if (computerNumber > userNumber) {
+            message = 'Il computer ha vinto!'
+            console.log(`Il computer ha vinto! il numero ${computerNumber} è maggiore di ${userNumber}`);
+        } else {
+            message = 'pareggio'
+            console.log(`Non ha vinto nessuno! Il numero ${userNumber} è uguale al numero ${computerNumber}`);
+        }
+        
+        eleOutput.innerHTML = message;
+        console.log(message);
+    }
+    
+);
 
